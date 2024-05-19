87ac9943c8d6ba3b729ef20a719d177b9e092bfe
Kateryna <kati.skrinnikova@gmail.com>
2024-05-19 14:50:52 +0300

    navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import { Heading } from 'components';
+// import { Heading } from 'components';
+
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Header } from './components';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('pages/Home'));
+const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 };
+{
+  /* <Heading title="Just do it!" />; */
+}
